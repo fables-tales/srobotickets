8ea557bc8c4640f1be420d99f6dcb90c80062ece
Sam Phippen <samphippen@googlemail.com>
2012-02-04 11:50:38 +0000

    Added a function to convert a team to a school
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- config/initializers/srobo.rb
+++ config/initializers/srobo.rb
@@ -6,3 +6,24 @@ require "srobo_ldap"
 
 SRoboLDAP.key = "osadifjoqijfqwoefjoqiefjoqi"
 SRoboLDAP.ldappwd = "oasifjasoidfja"
+
+
+def team_to_school(groups)
+    lines = open("teams.csv").read.strip.split("\n")
+    
+    
+    groups.each do |team|
+        lines.each do |line|
+            puts line
+            puts "team#{line.split(",")[0]}"
+            puts team
+            if "team#{line.split(",")[0]}".strip == team.strip
+                puts "bees"
+                return line.split(",")[1].strip
+            end
+        end
+    end
+    
+    return nil
+
+end

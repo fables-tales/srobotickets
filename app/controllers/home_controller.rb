c5f2c3d503892878882aa61ff38e2efdd8fc37c2
Sam Phippen <samphippen@googlemail.com>
2012-02-04 11:49:35 +0000

    Changed the data encoded in the qrcode to comply with DPA
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,14 +12,19 @@ class HomeController < ApplicationController
 
   def make_ticket
     @user_name = params[:user]
-    @details = SRoboLDAP.instance.ldap_user_details({"username" => "ticket-manager", "password" => SRoboLDAP.ldappwd}, @user_name)
-    @data_string = params[:user].lstrip.rstrip + ":" + @details[:cn] + ":" + @details[:sn] + ":" + @details[:mail]
-    digest = OpenSSL::Digest.new("sha256")
-    key = SRoboLDAP.key
-    @hmac = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new("sha256"), SRoboLDAP.key, @data_string))
-    
-    respond_to do |format|
-        format.html
+    @check = params[:check]
+    if @check
+        @details = SRoboLDAP.instance.ldap_user_details({"username" => "ticket-manager", "password" => SRoboLDAP.ldappwd}, @user_name)
+        @data_string = params[:user].lstrip.rstrip + ":" + @details[:cn] + ":" + @details[:sn] + ":" + @details[:mail]
+        digest = OpenSSL::Digest.new("sha256")
+        key = SRoboLDAP.key
+        @hmac = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new("sha256"), SRoboLDAP.key, @data_string))
+        
+        respond_to do |format|
+            format.html
+        end
+    else
+        redirect_to "/"
     end
   end
 
